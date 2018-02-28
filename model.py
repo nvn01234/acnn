bd4622b316f4d27b7744c1b3619e3a7a6b81b030
nhatnv <nguyenvannhat152@gmail.com>
2018-02-28 15:14:12 +0900

    .

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+
 # 05-13 11:27 Epoch: 1 Train: 21.86% Test: 47.67%
 # 05-13 11:28 Epoch: 2 Train: 45.29% Test: 59.96%
 # 05-13 11:29 Epoch: 3 Train: 55.65% Test: 66.56%
@@ -18,192 +19,188 @@ import tensorflow as tf
 # 05-13 11:40 Epoch: 16 Train: 93.14% Test: 70.59%
 
 def _bi_rnn(config, inputs, seq_len, is_training=True, scope=None):
-  '''
-  return value:
-    output:(output_fw, output_bw) [batch_size, max_time, hidden_size]
-    state: (state_fw, state_bw) ([batch_size, hidden_size], ...) len() == num_layers
-  '''
-  def gru_cell():
-    return tf.contrib.rnn.GRUCell(config.hidden_size)
-  cell = gru_cell
-  if is_training and config.keep_prob < 1:
-    def cell():
-      return tf.contrib.rnn.DropoutWrapper(
-            gru_cell(), output_keep_prob=config.keep_prob)
-  cell_fw = tf.contrib.rnn.MultiRNNCell([cell() for _ in range(config.num_layers)] )
-  cell_bw = tf.contrib.rnn.MultiRNNCell([cell() for _ in range(config.num_layers)] )
+    '''
+    return value:
+      output:(output_fw, output_bw) [batch_size, max_time, hidden_size]
+      state: (state_fw, state_bw) ([batch_size, hidden_size], ...) len() == num_layers
+    '''
 
-  return tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, inputs, 
-                          sequence_length=seq_len, dtype=tf.float32, scope=scope)
+    def gru_cell():
+        return tf.contrib.rnn.GRUCell(config.hidden_size)
+
+    cell = gru_cell
+    if is_training and config.keep_prob < 1:
+        def cell():
+            return tf.contrib.rnn.DropoutWrapper(
+                gru_cell(), output_keep_prob=config.keep_prob)
+    cell_fw = tf.contrib.rnn.MultiRNNCell([cell() for _ in range(config.num_layers)])
+    cell_bw = tf.contrib.rnn.MultiRNNCell([cell() for _ in range(config.num_layers)])
+
+    return tf.nn.bidirectional_dynamic_rnn(cell_fw, cell_bw, inputs,
+                                           sequence_length=seq_len, dtype=tf.float32, scope=scope)
 
 
 class Model(object):
-  def __init__(self, config, embeddings, is_training=True):
-    bz = config.batch_size
-    dw = config.embedding_size
-    dp = config.pos_embed_size
-    np = config.pos_embed_num
-    n = config.max_len
-    d = dw+2*dp
-    hz = config.hidden_size
-    # k = config.slide_window
-    dc = config.num_filters
-    nr = config.classnum # number of relations
-    keep_prob = config.keep_prob
+    def __init__(self, config, embeddings, is_training=True):
+        bz = config.batch_size
+        dw = config.embedding_size
+        dp = config.pos_embed_size
+        np = config.pos_embed_num
+        n = config.max_len
+        d = dw + 2 * dp
+        hz = config.hidden_size
+        # k = config.slide_window
+        dc = config.num_filters
+        nr = config.classnum  # number of relations
+        keep_prob = config.keep_prob
 
-    # input
-    in_x = tf.placeholder(dtype=tf.int32, shape=[bz,n], name='in_x') # sentences
-    in_len = tf.placeholder(dtype=tf.int32, shape=[bz], name='in_len') # real length of each sentences
-    in_e1 = tf.placeholder(dtype=tf.int32, shape=[bz, 3], name='in_e1')
-    in_e2 = tf.placeholder(dtype=tf.int32, shape=[bz, 3], name='in_e2')
-    in_dist1 = tf.placeholder(dtype=tf.int32, shape=[bz,n], name='in_dist1')
-    in_dist2 = tf.placeholder(dtype=tf.int32, shape=[bz,n], name='in_dist2')
-    in_y = tf.placeholder(dtype=tf.int32, shape=[bz], name='in_y') # relations
-    
-    self.inputs = (in_x, in_len, in_e1, in_e2, in_dist1, in_dist2, in_y)
-    # self.inputs = (in_x, in_e1, in_e2, in_dist1, in_dist2, in_y)
-    
-    # embeddings
-    initializer = tf.truncated_normal_initializer(stddev=0.1)
-    embed = tf.get_variable(initializer=embeddings, dtype=tf.float32, name='word_embed')
-    pos1_embed = tf.get_variable(initializer=initializer,shape=[np, dp],name='position1_embed')
-    pos2_embed = tf.get_variable(initializer=initializer,shape=[np, dp],name='position2_embed')
-    rel_embed = tf.get_variable(initializer=initializer,shape=[nr, dc],name='relation_embed')
+        # input
+        in_x = tf.placeholder(dtype=tf.int32, shape=[bz, n], name='in_x')  # sentences
+        in_len = tf.placeholder(dtype=tf.int32, shape=[bz], name='in_len')  # real length of each sentences
+        in_e1 = tf.placeholder(dtype=tf.int32, shape=[bz, 3], name='in_e1')
+        in_e2 = tf.placeholder(dtype=tf.int32, shape=[bz, 3], name='in_e2')
+        in_dist1 = tf.placeholder(dtype=tf.int32, shape=[bz, n], name='in_dist1')
+        in_dist2 = tf.placeholder(dtype=tf.int32, shape=[bz, n], name='in_dist2')
+        in_y = tf.placeholder(dtype=tf.int32, shape=[bz], name='in_y')  # relations
 
-    # def slide_window(x, k):
-    #   hk = k // 2 # half k
-    #   x_pad = tf.pad(x, [[0,0], [hk,hk]], "CONSTANT")# bz, n+2*(k-1)
-    #   x_k = tf.map_fn(lambda i: x_pad[:, i:i+k], tf.range(n), dtype=tf.int32)
-    #   return tf.stack(tf.unstack(x_k), axis=1)# bz, n, k
-    
-    # x_3 = slide_window(in_x, 3)
-    
-    # embdding lookup
-    e1 = tf.nn.embedding_lookup(embed, in_e1, name='e1')# bz,dw
-    e2 = tf.nn.embedding_lookup(embed, in_e2, name='e2')# bz,dw
-    x = tf.nn.embedding_lookup(embed, in_x, name='x')   # bz,n,dw
-    # x_3 = tf.nn.embedding_lookup(embed, x_3, name='x_3')
-    dist1 = tf.nn.embedding_lookup(pos1_embed, in_dist1, name='dist1')#bz, n, k,dp
-    dist2 = tf.nn.embedding_lookup(pos2_embed, in_dist2, name='dist2')# bz, n, k,dp
-    # y = tf.nn.embedding_lookup(rel_embed, in_y, name='y')# bz, dc
-    x_concat = tf.concat([x, dist1, dist2], -1) # bz, n, d
+        self.inputs = (in_x, in_len, in_e1, in_e2, in_dist1, in_dist2, in_y)
+        # self.inputs = (in_x, in_e1, in_e2, in_dist1, in_dist2, in_y)
 
-    # # input attention
-    # x_3 = tf.reshape(x_3, [bz, n, 3*dw])
-    # A1 = tf.matmul(x_3, tf.reshape(e1, [bz, 3*dw, 1]))# bz, n, 1
-    # A2 = tf.matmul(x_3, tf.reshape(e2, [bz, 3*dw, 1]))
-    # A1 = tf.reshape(A1, [bz, n])
-    # A2 = tf.reshape(A2, [bz, n])
-    # alpha1 = tf.nn.softmax(A1)# bz, n
-    # alpha2 = tf.nn.softmax(A2)# bz, n
-    # alpha = (alpha1 + alpha2)/2
+        # embeddings
+        initializer = tf.truncated_normal_initializer(stddev=0.1)
+        embed = tf.get_variable(initializer=embeddings, dtype=tf.float32, name='word_embed')
+        pos1_embed = tf.get_variable(initializer=initializer, shape=[np, dp], name='position1_embed')
+        pos2_embed = tf.get_variable(initializer=initializer, shape=[np, dp], name='position2_embed')
+        rel_embed = tf.get_variable(initializer=initializer, shape=[nr, dc], name='relation_embed')
 
+        # def slide_window(x, k):
+        #   hk = k // 2 # half k
+        #   x_pad = tf.pad(x, [[0,0], [hk,hk]], "CONSTANT")# bz, n+2*(k-1)
+        #   x_k = tf.map_fn(lambda i: x_pad[:, i:i+k], tf.range(n), dtype=tf.int32)
+        #   return tf.stack(tf.unstack(x_k), axis=1)# bz, n, k
 
+        # x_3 = slide_window(in_x, 3)
 
-    # bidirectional rnn
-    output_rnn, state_rnn = _bi_rnn(config, x_concat, in_len, is_training, 'rnn')
-    x_rnn = tf.concat([output_rnn[0], output_rnn[1]], axis=2) # xi = (fw_hi, bw_hi), shape: (bz, n, hz)
-    d = hz*2 # dw+2*dp => hz*2
-    x_concat = tf.reshape(x_rnn, [bz,n,d,1])
-    # print(x_concat.get_shape())
-    # exit()
-    
+        # embdding lookup
+        e1 = tf.nn.embedding_lookup(embed, in_e1, name='e1')  # bz,dw
+        e2 = tf.nn.embedding_lookup(embed, in_e2, name='e2')  # bz,dw
+        x = tf.nn.embedding_lookup(embed, in_x, name='x')  # bz,n,dw
+        # x_3 = tf.nn.embedding_lookup(embed, x_3, name='x_3')
+        dist1 = tf.nn.embedding_lookup(pos1_embed, in_dist1, name='dist1')  # bz, n, k,dp
+        dist2 = tf.nn.embedding_lookup(pos2_embed, in_dist2, name='dist2')  # bz, n, k,dp
+        # y = tf.nn.embedding_lookup(rel_embed, in_y, name='y')# bz, dc
+        x_concat = tf.concat([x, dist1, dist2], -1)  # bz, n, d
 
-    # x_concat = tf.reshape(x_concat, [bz,n,d,1])
+        # # input attention
+        # x_3 = tf.reshape(x_3, [bz, n, 3*dw])
+        # A1 = tf.matmul(x_3, tf.reshape(e1, [bz, 3*dw, 1]))# bz, n, 1
+        # A2 = tf.matmul(x_3, tf.reshape(e2, [bz, 3*dw, 1]))
+        # A1 = tf.reshape(A1, [bz, n])
+        # A2 = tf.reshape(A2, [bz, n])
+        # alpha1 = tf.nn.softmax(A1)# bz, n
+        # alpha2 = tf.nn.softmax(A2)# bz, n
+        # alpha = (alpha1 + alpha2)/2
 
-    # convolution
-    # x: (batch_size, max_len, embdding_size, 1)
-    # w: (filter_size, embdding_size, 1, num_filters)
-    filter_sizes = [3, 4, 5]
-    pooled_outputs = []
-    if is_training and keep_prob < 1:
-      x_concat = tf.nn.dropout(x_concat, keep_prob)
+        # bidirectional rnn
+        output_rnn, state_rnn = _bi_rnn(config, x_concat, in_len, is_training, 'rnn')
+        x_rnn = tf.concat([output_rnn[0], output_rnn[1]], axis=2)  # xi = (fw_hi, bw_hi), shape: (bz, n, hz)
+        d = hz * 2  # dw+2*dp => hz*2
+        x_concat = tf.reshape(x_rnn, [bz, n, d, 1])
+        # print(x_concat.get_shape())
+        # exit()
 
-    for i, k in enumerate(filter_sizes):
-      with tf.variable_scope("conv-%d" % k):# , reuse=False
-        w = tf.get_variable(initializer=initializer,shape=[k, d, 1, dc],name='weight')
-        b = tf.get_variable(initializer=initializer,shape=[dc],name='bias')
-        conv = tf.nn.conv2d(x_concat, w, strides=[1,1,d,1],padding="SAME")
+        # x_concat = tf.reshape(x_concat, [bz,n,d,1])
 
-        h = tf.nn.tanh(tf.nn.bias_add(conv,b),name="h") # bz, n, 1, dc
+        # convolution
+        # x: (batch_size, max_len, embdding_size, 1)
+        # w: (filter_size, embdding_size, 1, num_filters)
+        filter_sizes = [3, 4, 5]
+        pooled_outputs = []
+        if is_training and keep_prob < 1:
+            x_concat = tf.nn.dropout(x_concat, keep_prob)
 
-        # conv = tf.multiply(tf.reshape(h, [bz, n, dc]), tf.reshape(alpha, [bz, n, 1])) # bz, n, dc
-        # h = tf.reshape(conv, [bz, n, 1, dc])
+        for i, k in enumerate(filter_sizes):
+            with tf.variable_scope("conv-%d" % k):  # , reuse=False
+                w = tf.get_variable(initializer=initializer, shape=[k, d, 1, dc], name='weight')
+                b = tf.get_variable(initializer=initializer, shape=[dc], name='bias')
+                conv = tf.nn.conv2d(x_concat, w, strides=[1, 1, d, 1], padding="SAME")
 
-        # U: [dc, dc]
-        R = tf.reshape(h, [bz, n, dc])
-        U = tf.get_variable(initializer=initializer,shape=[dc,dc],name='U')
-        G = tf.matmul(tf.reshape(R, [bz*n, dc]), U)
-        G = tf.matmul(G, tf.transpose(rel_embed)) 
-        G = tf.reshape(G, [bz, n, nr])
-        AP = tf.nn.softmax(G, dim=1)# attention pooling tensor
+                h = tf.nn.tanh(tf.nn.bias_add(conv, b), name="h")  # bz, n, 1, dc
 
-        wo = tf.matmul(tf.transpose(R, perm=[0, 2, 1]),AP)# (bz, dc, nr)
-        wo = tf.reduce_max(wo, axis=-1) # (bz, dc)
-        pooled_outputs.append(wo)
+                # conv = tf.multiply(tf.reshape(h, [bz, n, dc]), tf.reshape(alpha, [bz, n, 1])) # bz, n, dc
+                # h = tf.reshape(conv, [bz, n, 1, dc])
 
-        # # max pooling
-        # pooled = tf.nn.max_pool(h,
-        #                     ksize=[1,n,1,1],
-        #                     strides=[1,n,1,1],
-        #                     padding="SAME"
-        #       )
-        # pooled_outputs.append(pooled)
-    # h_pool = tf.concat(pooled_outputs, 3)
-    # h_pool_flat = tf.reshape(h_pool,[-1,dc*len(filter_sizes)])
-    h_pool_flat = tf.concat(pooled_outputs, -1)
+                # U: [dc, dc]
+                R = tf.reshape(h, [bz, n, dc])
+                U = tf.get_variable(initializer=initializer, shape=[dc, dc], name='U')
+                G = tf.matmul(tf.reshape(R, [bz * n, dc]), U)
+                G = tf.matmul(G, tf.transpose(rel_embed))
+                G = tf.reshape(G, [bz, n, nr])
+                AP = tf.nn.softmax(G, dim=1)  # attention pooling tensor
 
-    # e embdding
-    e_flat = tf.concat([e1, e2], 2)
-    e_flat = tf.reshape(e_flat,[-1,dw*6])
-    all_flat = tf.concat([h_pool_flat, e_flat],1)
-    # h_pool_flat = all_flat
-    h_pool_flat = tf.reshape(all_flat,[-1,dc*len(filter_sizes) + dw * 6])
+                wo = tf.matmul(tf.transpose(R, perm=[0, 2, 1]), AP)  # (bz, dc, nr)
+                wo = tf.reduce_max(wo, axis=-1)  # (bz, dc)
+                pooled_outputs.append(wo)
 
+                # # max pooling
+                # pooled = tf.nn.max_pool(h,
+                #                     ksize=[1,n,1,1],
+                #                     strides=[1,n,1,1],
+                #                     padding="SAME"
+                #       )
+                # pooled_outputs.append(pooled)
+        # h_pool = tf.concat(pooled_outputs, 3)
+        # h_pool_flat = tf.reshape(h_pool,[-1,dc*len(filter_sizes)])
+        h_pool_flat = tf.concat(pooled_outputs, -1)
 
-    if is_training and keep_prob < 1:
-      h_pool_flat = tf.nn.dropout(h_pool_flat, keep_prob)
-    
-    # output
-    W_o = tf.get_variable(initializer=initializer,shape=[dc*len(filter_sizes) + dw * 6,nr],name='w_o')
-    b_o = tf.get_variable(initializer=initializer,shape=[nr],name='b_o')
-    scores = tf.nn.xw_plus_b(h_pool_flat,W_o,b_o,name="scores")
-    predict = tf.argmax(scores,1,name="predictions")
-    predict = tf.cast(predict, dtype=tf.int32)
-    acc = tf.reduce_sum(tf.cast(tf.equal(predict, in_y), dtype=tf.int32))
-    self.predict = predict
-    self.acc = acc
+        # e embdding
+        e_flat = tf.concat([e1, e2], 2)
+        e_flat = tf.reshape(e_flat, [-1, dw * 6])
+        all_flat = tf.concat([h_pool_flat, e_flat], 1)
+        # h_pool_flat = all_flat
+        h_pool_flat = tf.reshape(all_flat, [-1, dc * len(filter_sizes) + dw * 6])
 
-    loss = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(logits=scores, 
-                                              labels=tf.one_hot(in_y, nr))
-    )
-    l2_loss = tf.nn.l2_loss(W_o)
-    l2_loss += tf.nn.l2_loss(b_o)
-    l2_loss = config.l2_reg_lambda * l2_loss
-    
-    self.loss = loss + l2_loss
+        if is_training and keep_prob < 1:
+            h_pool_flat = tf.nn.dropout(h_pool_flat, keep_prob)
 
-    if not is_training:
-      return
+        # output
+        W_o = tf.get_variable(initializer=initializer, shape=[dc * len(filter_sizes) + dw * 6, nr], name='w_o')
+        b_o = tf.get_variable(initializer=initializer, shape=[nr], name='b_o')
+        scores = tf.nn.xw_plus_b(h_pool_flat, W_o, b_o, name="scores")
+        predict = tf.argmax(scores, 1, name="predictions")
+        predict = tf.cast(predict, dtype=tf.int32)
+        acc = tf.reduce_sum(tf.cast(tf.equal(predict, in_y), dtype=tf.int32))
+        self.predict = predict
+        self.acc = acc
 
-    # optimizer 
-    # optimizer = tf.train.GradientDescentOptimizer(config.learning_rate)
-    optimizer = tf.train.AdamOptimizer(config.learning_rate)
-    # optimizer2 = tf.train.AdamOptimizer(config.learning_rate2)
+        loss = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(logits=scores,
+                                                    labels=tf.one_hot(in_y, nr))
+        )
+        l2_loss = tf.nn.l2_loss(W_o)
+        l2_loss += tf.nn.l2_loss(b_o)
+        l2_loss = config.l2_reg_lambda * l2_loss
 
-    # tvars = tf.trainable_variables()
-    # grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars),
-    #                                   config.grad_clipping)
-    # capped_gvs = zip(grads, tvars)
+        self.loss = loss + l2_loss
 
-    # tf.logging.set_verbosity(tf.logging.ERROR)
-    global_step = tf.Variable(0, trainable=False, name='global_step')
-    # train_op = optimizer.apply_gradients(capped_gvs, global_step=global_step)
-    # reg_op = optimizer2.minimize(l2_loss)
+        if not is_training:
+            return
 
-    self.train_op = optimizer.minimize(self.loss)
-    self.reg_op = tf.no_op()
-    self.global_step = global_step
+        # optimizer
+        # optimizer = tf.train.GradientDescentOptimizer(config.learning_rate)
+        optimizer = tf.train.AdamOptimizer(config.learning_rate)
+        # optimizer2 = tf.train.AdamOptimizer(config.learning_rate2)
 
-    
+        # tvars = tf.trainable_variables()
+        # grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars),
+        #                                   config.grad_clipping)
+        # capped_gvs = zip(grads, tvars)
+
+        # tf.logging.set_verbosity(tf.logging.ERROR)
+        global_step = tf.Variable(0, trainable=False, name='global_step')
+        # train_op = optimizer.apply_gradients(capped_gvs, global_step=global_step)
+        # reg_op = optimizer2.minimize(l2_loss)
+
+        self.train_op = optimizer.minimize(self.loss)
+        self.reg_op = tf.no_op()
+        self.global_step = global_step
--- utils.py
+++ utils.py
@@ -2,152 +2,153 @@ import numpy as np
 import logging
 from collections import Counter
 
-def load_data(file):
-  sentences = []
-  relations = []
-  e1_pos = []
-  e2_pos = []
 
-  with open(file, 'r') as f:
-    for line in f.readlines():
-      line = line.strip().lower().split()
-      relations.append(int(line[0]))
-      e1_pos.append( (int(line[1]), int(line[2])) ) # (start_pos, end_pos)
-      e2_pos.append( (int(line[3]), int(line[4])) ) # (start_pos, end_pos)
-      sentences.append(line[5:])
-  
-  return sentences, relations, e1_pos, e2_pos
+def load_data(file):
+    sentences = []
+    relations = []
+    e1_pos = []
+    e2_pos = []
+
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            line = line.strip().lower().split()
+            relations.append(int(line[0]))
+            e1_pos.append((int(line[1]), int(line[2])))  # (start_pos, end_pos)
+            e2_pos.append((int(line[3]), int(line[4])))  # (start_pos, end_pos)
+            sentences.append(line[5:])
+
+    return sentences, relations, e1_pos, e2_pos
+
 
 def build_dict(sentences):
-  word_count = Counter()
-  for sent in sentences:
-    for w in sent:
-      word_count[w] += 1
+    word_count = Counter()
+    for sent in sentences:
+        for w in sent:
+            word_count[w] += 1
 
-  ls = word_count.most_common()
-  
-  # leave 0 to PAD
-  return {w[0]: index + 1 for (index, w) in enumerate(ls)}
+    ls = word_count.most_common()
+
+    # leave 0 to PAD
+    return {w[0]: index + 1 for (index, w) in enumerate(ls)}
+
 
 def load_embedding(config, word_dict):
-  emb_file = config.embedding_file
-  emb_vocab = config.embedding_vocab
+    emb_file = config.embedding_file
+    emb_vocab = config.embedding_vocab
 
-  vocab = {}
-  with open(emb_vocab, 'r') as f:
-    for id, w in enumerate(f.readlines()):
-      w = w.strip().lower()
-      vocab[w] = id
-  
-  f = open(emb_file, 'r')
-  embed = f.readlines()
+    vocab = {}
+    with open(emb_vocab, 'r') as f:
+        for id, w in enumerate(f.readlines()):
+            w = w.strip().lower()
+            vocab[w] = id
 
-  dim = len(embed[0].split())
-  num_words = len(word_dict) + 1
-  embeddings = np.random.uniform(-0.01, 0.01, size=(num_words, dim))
-  config.embedding_size = dim
+    f = open(emb_file, 'r')
+    embed = f.readlines()
 
-  pre_trained = 0
-  for w in vocab.keys():
-    if w in word_dict:
-      embeddings[word_dict[w]] = [float(x) for x in embed[vocab[w]].split()]
-      pre_trained += 1
-  embeddings[0] = np.zeros((dim))
+    dim = len(embed[0].split())
+    num_words = len(word_dict) + 1
+    embeddings = np.random.uniform(-0.01, 0.01, size=(num_words, dim))
+    config.embedding_size = dim
 
-  logging.info('embeddings: %.2f%%(pre_trained) unknown: %d' %(pre_trained/num_words*100, num_words-pre_trained))
+    pre_trained = 0
+    for w in vocab.keys():
+        if w in word_dict:
+            embeddings[word_dict[w]] = [float(x) for x in embed[vocab[w]].split()]
+            pre_trained += 1
+    embeddings[0] = np.zeros((dim))
 
-  f.close()
-  return embeddings.astype(np.float32)
+    logging.info(
+        'embeddings: %.2f%%(pre_trained) unknown: %d' % (pre_trained / num_words * 100, num_words - pre_trained))
+
+    f.close()
+    return embeddings.astype(np.float32)
+
 
 def vectorize(data, word_dict, max_len):
-  sentences, relations, e1_pos, e2_pos = data
+    sentences, relations, e1_pos, e2_pos = data
 
-  # replace word with word-id
-  # sents_vec = []
-  e1_vec = []
-  e2_vec = []
+    # replace word with word-id
+    # sents_vec = []
+    e1_vec = []
+    e2_vec = []
 
-  
-  num_data = len(sentences)
-  sents_vec = np.zeros((num_data, max_len), dtype=int)
-  sents_len = []
-  # mb_len1 = [len(seq) for seq in mb_x1]
-  logging.debug('data shape: (%d, %d)' % (num_data, max_len))
-  
+    num_data = len(sentences)
+    sents_vec = np.zeros((num_data, max_len), dtype=int)
+    sents_len = []
+    # mb_len1 = [len(seq) for seq in mb_x1]
+    logging.debug('data shape: (%d, %d)' % (num_data, max_len))
 
-  for idx, (sent, pos1, pos2) in enumerate(zip(sentences, e1_pos, e2_pos)):
-    vec = [word_dict[w] if w in word_dict else 0 for w in sent]
-    sents_vec[idx, :len(vec)] = vec
-    sents_len.append(len(vec))
-    # # log e1 and e2 if e1 or e2 is a phrase
-    # if pos1[0]!=pos1[1] or pos2[0]!=pos2[1]:
-    #   s_e1 = ''
-    #   for w in sent[pos1[0] : pos1[1]+1]:
-    #     s_e1 += w + ' '
-    #   s_e2 = ''
-    #   for w in sent[pos2[0] : pos2[1]+1]:
-    #     s_e2 += w + ' '
-    #   logging.debug("%s - %s" % (s_e1, s_e2))
+    for idx, (sent, pos1, pos2) in enumerate(zip(sentences, e1_pos, e2_pos)):
+        vec = [word_dict[w] if w in word_dict else 0 for w in sent]
+        sents_vec[idx, :len(vec)] = vec
+        sents_len.append(len(vec))
 
-    # # the entire e1 and e2 phrase
-    # e1_vec.append(vec[pos1[0] : pos1[1]+1])
-    # e2_vec.append(vec[pos2[0] : pos2[1]+1])
+        # # log e1 and e2 if e1 or e2 is a phrase
+        # if pos1[0]!=pos1[1] or pos2[0]!=pos2[1]:
+        #   s_e1 = ''
+        #   for w in sent[pos1[0] : pos1[1]+1]:
+        #     s_e1 += w + ' '
+        #   s_e2 = ''
+        #   for w in sent[pos2[0] : pos2[1]+1]:
+        #     s_e2 += w + ' '
+        #   logging.debug("%s - %s" % (s_e1, s_e2))
 
-    # # last word of e1 and e2
-    # e1_vec.append(vec[pos1[1]])
-    # e2_vec.append(vec[pos2[1]])
+        # # the entire e1 and e2 phrase
+        # e1_vec.append(vec[pos1[0] : pos1[1]+1])
+        # e2_vec.append(vec[pos2[0] : pos2[1]+1])
 
-    # first position of e1 and e2
-    # e1_vec.append(pos1[0])
-    # e2_vec.append(pos2[0])
+        # # last word of e1 and e2
+        # e1_vec.append(vec[pos1[1]])
+        # e2_vec.append(vec[pos2[1]])
 
-    # 3 word window of e1 or e2
-    def e_vec_f(pos):
-      e_tmp = []
-      i = pos[0]
-      if i > 0:
-        e_tmp.append(vec[i-1])
-      else:
-        e_tmp.append(vec[0])
-      
-      e_tmp.append(vec[i])
+        # first position of e1 and e2
+        # e1_vec.append(pos1[0])
+        # e2_vec.append(pos2[0])
 
-      if i < len(vec)-1:
-        e_tmp.append(vec[i+1])
-      else:
-        e_tmp.append(vec[i])
-      
-      return e_tmp
-    e1_vec.append(e_vec_f(pos1))
-    e2_vec.append(e_vec_f(pos2))
+        # 3 word window of e1 or e2
+        def e_vec_f(pos):
+            e_tmp = []
+            i = pos[0]
+            if i > 0:
+                e_tmp.append(vec[i - 1])
+            else:
+                e_tmp.append(vec[0])
 
-    
+            e_tmp.append(vec[i])
 
+            if i < len(vec) - 1:
+                e_tmp.append(vec[i + 1])
+            else:
+                e_tmp.append(vec[i])
 
+            return e_tmp
 
-  # compute relative distance
-  dist1 = []
-  dist2 = []
+        e1_vec.append(e_vec_f(pos1))
+        e2_vec.append(e_vec_f(pos2))
 
-  
+    # compute relative distance
+    dist1 = []
+    dist2 = []
 
-  for sent, p1, p2 in zip(sents_vec, e1_pos, e2_pos):
-    # current word position - last word position of e1 or e2
-    dist1.append([pos(p1[1]-idx) for idx, _ in enumerate(sent)])
-    dist2.append([pos(p2[1]-idx) for idx, _ in enumerate(sent)])
-  
-  return sents_vec, sents_len, relations, e1_vec, e2_vec, dist1, dist2
+    for sent, p1, p2 in zip(sents_vec, e1_pos, e2_pos):
+        # current word position - last word position of e1 or e2
+        dist1.append([pos(p1[1] - idx) for idx, _ in enumerate(sent)])
+        dist2.append([pos(p2[1] - idx) for idx, _ in enumerate(sent)])
+
+    return sents_vec, sents_len, relations, e1_vec, e2_vec, dist1, dist2
+
 
 def pos(x):
-  '''
-  map the relative distance between [0, 123)
-  '''
-  if x < -60:
-      return 0
-  if x >= -60 and x <= 60:
-      return x + 61
-  if x > 60:
-      return 122
+    '''
+    map the relative distance between [0, 123)
+    '''
+    if x < -60:
+        return 0
+    if x >= -60 and x <= 60:
+        return x + 61
+    if x > 60:
+        return 122
+
 
 def batch_iter(data, batch_size, shuffle=True):
     """
@@ -163,17 +164,16 @@ def batch_iter(data, batch_size, shuffle=True):
     # logging.info("Generating batches.. Total # of batches %d" % batches_per_epoch)
 
     if shuffle:
-      indices = np.random.permutation(np.arange(data_size))
-      shuffled_data = data[indices]
+        indices = np.random.permutation(np.arange(data_size))
+        shuffled_data = data[indices]
     else:
-      shuffled_data = data
+        shuffled_data = data
     for batch_num in range(batches_per_epoch):
-      start_index = batch_num * batch_size
-      end_index = min((batch_num + 1) * batch_size, data_size)
-      yield shuffled_data[start_index:end_index]
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        yield shuffled_data[start_index:end_index]
+
 
 if __name__ == '__main__':
-  for item in load_data('data/test.txt'):
-    print(item)
-
-
+    for item in load_data('data/test.txt'):
+        print(item)
